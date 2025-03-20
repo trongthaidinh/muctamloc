b072a6990820839e7e9154b59b2641d5906c6dc9
thaidinh <thai562003@gmail.com>
2025-03-20 07:40:43 +0700

    Update Admin Dashboard

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,14 +3,39 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
+use App\Models\Cart;
+use App\Models\Category;
+use App\Models\Customer;
+use App\Models\Message;
+use App\Models\News;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+        $totalUsers = User::count();  // Fetch total users
+        $totalProducts = Product::count();  // Fetch total products
+        $totalOrders = Cart::count();  // Fetch total orders
+        $totalFeedback = Message::count();  // Fetch total feedback
+        $totalCustomers = Customer::count();  // Fetch total customers
+        $totalNews = News::count();  // Fetch total news
+        $totalBanners = Banner::count();  // Fetch total banners
+        $totalCategories = Category::count();  // Fetch total categories
+
         return view('admin.home', [
-            'title' => 'Trang Quản Trị '
+            'title' => 'Trang Quản Trị',
+            'totalUsers' => $totalUsers,
+            'totalProducts' => $totalProducts,
+            'totalOrders' => $totalOrders,
+            'totalFeedback' => $totalFeedback,
+            'totalCustomers' => $totalCustomers,
+            'totalNews' => $totalNews,
+            'totalBanners' => $totalBanners,
+            'totalCategories' => $totalCategories,
         ]);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,9 +1,100 @@
 @extends('admin.main')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <img src="{{ asset('3.jpg') }}" style="width: 100%" />
+<div class="container-fluid">
+    <div class="row mb-3">
+        <div class="col-lg-3 col-6">
+            <a href="/admin/user/list   " class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $totalUsers }}</h3>
+                    <p>Tổng Người Dùng</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-users"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-6">
+            <a href="/admin/products/list" class="small-box bg-success">
+                <div class="inner">
+                    <h3>{{ $totalProducts }}</h3>
+                    <p>Tổng Sản Phẩm</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-box"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-6">
+            <a href="/admin/categorys/list" class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ $totalCategories }}</h3>
+                    <p>Tổng Danh Mục</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-list"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-6">
+            <a href="/admin/cart" class="small-box bg-warning">
+                <div class="inner">
+                    <h3>{{ $totalOrders }}</h3>
+                    <p>Tổng Đơn Hàng</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-shopping-cart"></i>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col-lg-3 col-6">
+            <a href="/admin/banners/add" class="small-box bg-primary">
+                <div class="inner">
+                    <h3>{{ $totalBanners }}</h3>
+                    <p>Tổng Banner</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-bullseye"></i>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <a href="/admin" class="small-box bg-secondary">
+                <div class="inner">
+                    <h3>{{ $totalCustomers }}</h3>
+                    <p>Tổng Khách Hàng</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-user"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-6">
+            <a href="/admin/news/list" class="small-box bg-info">
+                <div class="inner">
+                    <h3>{{ $totalNews }}</h3>
+                    <p>Tổng Tin Tức</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-newspaper"></i>
+                </div>
+            </a>
+        </div>
+        <div class="col-lg-3 col-6">
+            <a href="/admin/messages/list" class="small-box bg-danger">
+                <div class="inner">
+                    <h3>{{ $totalFeedback }}</h3>
+                    <p>Tổng Phản Hồi</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-comments"></i>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 @endsection
